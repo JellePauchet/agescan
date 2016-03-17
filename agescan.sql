4a93e956f6110d7290dfc57dd900d2ba038e27f1
MauroLambrechts <mauro.lambrechts1@gmail.com>
2016-03-17 12:10:09 +0100

    sql2

--- agescan.sql
+++ agescan.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 17 mrt 2016 om 11:40
+-- Gegenereerd op: 17 mrt 2016 om 12:09
 -- Serverversie: 10.1.9-MariaDB
 -- PHP-versie: 5.6.15
 
@@ -34,6 +34,14 @@ CREATE TABLE `evenement` (
   `MaxAanwezigen` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `evenement`
+--
+
+INSERT INTO `evenement` (`IDEvenement`, `Naam`, `Organisator`, `MinLeeftijd`, `MaxAanwezigen`) VALUES
+(1, 'Poerkwaspi 2016', 'Chiro Schelle', 16, 1000),
+(2, 'Tomorrowland', 'Boom', 18, 50000);
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +54,15 @@ CREATE TABLE `feestgangers` (
   `RRNR` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `feestgangers`
+--
+
+INSERT INTO `feestgangers` (`IDFeestgangers`, `Leeftijd`, `RRNR`) VALUES
+(1, 20, 565846816),
+(2, 35, 8378773),
+(3, 12, 737888);
+
 -- --------------------------------------------------------
 
 --
@@ -57,6 +74,15 @@ CREATE TABLE `feestgangersopevenement` (
   `IDEvenement` int(255) NOT NULL,
   `IDFeestgangers` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `feestgangersopevenement`
+--
+
+INSERT INTO `feestgangersopevenement` (`IDFOE`, `IDEvenement`, `IDFeestgangers`) VALUES
+(1, 1, 2),
+(2, 2, 3),
+(3, 1, 3);
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -88,17 +114,17 @@ ALTER TABLE `feestgangersopevenement`
 -- AUTO_INCREMENT voor een tabel `evenement`
 --
 ALTER TABLE `evenement`
-  MODIFY `IDEvenement` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `IDEvenement` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT voor een tabel `feestgangers`
 --
 ALTER TABLE `feestgangers`
-  MODIFY `IDFeestgangers` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `IDFeestgangers` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT voor een tabel `feestgangersopevenement`
 --
 ALTER TABLE `feestgangersopevenement`
-  MODIFY `IDFOE` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `IDFOE` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- views/scan.php
+++ views/scan.php
@@ -19,5 +19,4 @@
 	<script type='text/javascript'  src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 	<script type='text/javascript' src="http://ajax.googleapis.com/ajax/libs/angularjs/1.0.4/angular.js"></script>
 	<script type='text/javascript' src="js/AgeScan.js"></script>
-
 </body>
