6bc1e65b5252b581fd182707ea697f4c35b0c336
samme robbroeckx <samme robbroeckx>
2016-03-17 12:56:00 +0100

    bootstrap

--- controllers/linkdb.php
+++ controllers/linkdb.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "sjatoo_kassa", "^xiDOx(#-S8Z", "sjatoo_kassatelling");
+$conn = new mysqli("localhost","root","","agescan");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
--- index.php
+++ index.php
@@ -1,3 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>AgeScan</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	</head>
+	<body>
+	
+		<div class="navbar navbar-inverse navbar-static-top">
+			<div class="container">
+				<div class="navbar-header">
+				<a href="#" class="navbar-brand">AgeScan</a>
+					<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+				
+				<div class="collapse navbar-collapse navHeaderCollapse">
+				
+					<ul class="nav navbar-nav navbar-right">
+					
+						<li class="active"><a href="index.html">Home</a></li>				
+					</ul>
+				
+				</div>
+			</div>
+		</div>
+		
+		<div class="container">
 <?php
 	error_reporting( E_ALL );
 	ini_set( "display_errors", 1 );
@@ -19,3 +56,16 @@
 	$pagina = include_once "views/pagina.php";
 	echo $pagina;
 ?>
+
+</div>
+		
+		<div class="navbar navbar-default navbar-fixed-bottom">
+			<div class="container">
+				<p class="navbar-text">Deze site is gemaakt door <a href="http://teamcrap.be">Team Crap</a></p>
+			</div>
+		</div>
+		
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script src="js/bootstrap.js"></script>
+	</body>
+</html>
