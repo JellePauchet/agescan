551def464d324fd6138bd066cd4f18431fb88cfb
mat.boen <mat.boen@gmail.com>
2016-03-18 10:46:22 +0100

    new.php added

---
+++ views/new.php
@@ -0,0 +1,19 @@
+<body>
+	<h1>Voeg een nieuw evenement toe</h1>
+	<div>
+		<form autocomplete="off">
+			<input id ="Naam" />
+			<input id ="Organisator" />
+			<input id ="MinLeeftijd" />
+			<input id ="MaxAanwezigen" />
+			<input type="submit" id="submit" value="Submit" />
+		</form>
+	</div>
+	
+	<script type='text/javascript'  src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script type='text/javascript' src="http://ajax.googleapis.com/ajax/libs/angularjs/1.0.4/angular.js"></script>
+	<script type='text/javascript' src="js/AgeScan.js"></script>
+
+
+	
+</body>
