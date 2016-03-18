cac9a58b4caa59e3b6d204fa63e02c5a69b13f05
samme robbroeckx <samme robbroeckx>
2016-03-18 13:05:21 +0100

    form recht getrokken

--- views/new.php
+++ views/new.php
@@ -19,27 +19,27 @@
 <center>
 	<h1>Voeg een nieuw evenement toe</h1>
 	<div>
-
-	<form role="form" autocomplete="off">
-  <div class="form-group">
-    <label for="Naam">Naam:</label>
-    <input type="name" class="form-control" id="Naam">
-  </div>
-  <div class="form-group">
-    <label for="Organisator">Organisator:</label>
-    <input type="name" class="form-control" id="Organisator">
-  </div>
-  <div class="form-group">
-    <label for="MinLeeftijd">Minimum Leeftijd:</label>
-    <input type="number" class="form-control" id="MinLeeftijd">
-  </div>
-  <div class="form-group">
-    <label for="MaxAanwezigen">Maximum Leeftijd:</label>
-    <input type="number" class="form-control" id="MaxAanwezigen">
-  </div>
-  
-  <button type="submit" id="submit" class="btn btn-default">Submit</button>
-</form>
-
+		<form action="existing.php" method="post" autocomplete="off">
+			<table>
+				<tr>
+					<td width="180px">Naam evenement</td>
+					<td><input name ="Naam" size="35"/></td>
+				</tr>
+				<tr>
+					<td>Naam organisator</td>
+					<td><input name ="Organisator" size="35"/></td>
+				</tr>
+				<tr>
+					<td>Minimum leeftijd</td>
+					<td><input type="number" name ="MinLeeftijd" size="35"/></td>
+				</tr>
+				<tr>
+					<td>Maximum aanwezigen</td>
+					<td><input type="number" name ="MaxAanwezigen" size="35"/></td>
+				</tr>
+			</table><br>
+			<input type="submit" id="submit" value="Opslaan en verzenden" />
+		</form><br>
+		<p>Klik op "opslaan en verzenden" om uw uw nieuw evenement te starten</p>
 	</div>
 </center>
--- views/start.php
+++ views/start.php
@@ -4,8 +4,4 @@
 <h1 id="AgeScan" class="text-center">AgeScan</h1>
 <form action="?pagina=new" method="POST"><button type="submit" class="btn btn-primary" aria-haspopup="true" aria-expanded="false">Nieuw Evenement</button></form>
 
-<<<<<<< HEAD
-<button type="button" class="btn btn-succes" aria-haspopup="true" aria-expanded="false">Bestaand Evenement</button>
-=======
 <form action="?pagina=existing" method="POST"><button type="submit" class="btn btn-danger" aria-haspopup="true" aria-expanded="false">Bestaand Evenement</button></form>
->>>>>>> d2ea4183169d5520a4d333605c61e397a2468894
