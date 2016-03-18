dde8dd11617573c2ef3ec40b771d16f219a90aab
Conner Maes <Conner Maes>
2016-03-18 12:31:57 +0100

    start test

--- views/start.php
+++ views/start.php
@@ -4,4 +4,4 @@
 
 <form action="?pagina=new" method="POST"><button type="submit" class="btn btn-primary" aria-haspopup="true" aria-expanded="false">Nieuw Evenement</button></form>
 
-<button type="button" class="btn btn-danger" aria-haspopup="true" aria-expanded="false">Bestaand Evenement</button>
+<button type="button" class="btn btn-succes" aria-haspopup="true" aria-expanded="false">Bestaand Evenement</button>
