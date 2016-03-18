690fcd7c7a907d3236d7212b9619c1b540d73224
Conner Maes <Conner Maes>
2016-03-18 13:37:34 +0100

    form op niew evenement

--- views/new.php
+++ views/new.php
@@ -16,30 +16,37 @@
 	}
 ?>
 
+
+
 <center>
 	<h1>Voeg een nieuw evenement toe</h1>
-	<div>
-		<form action="existing.php" method="post" autocomplete="off">
-			<table>
-				<tr>
-					<td width="180px">Naam evenement</td>
-					<td><input name ="Naam" size="35"/></td>
-				</tr>
-				<tr>
-					<td>Naam organisator</td>
-					<td><input name ="Organisator" size="35"/></td>
-				</tr>
-				<tr>
-					<td>Minimum leeftijd</td>
-					<td><input type="number" name ="MinLeeftijd" size="35"/></td>
-				</tr>
-				<tr>
-					<td>Maximum aanwezigen</td>
-					<td><input type="number" name ="MaxAanwezigen" size="35"/></td>
-				</tr>
-			</table><br>
-			<input type="submit" id="submit" value="Opslaan en verzenden" />
-		</form><br>
+		<div>
+			<form class="form-inline" role="form" action="existing.php" method="post" autocomplete="off">
+				<div class="form-group">
+					<label for="Naam">Naam evenement:</label>
+				<div class="col-sm-8">
+					<input type="name" class="form-control" id="Naam">
+				</div>
+				</div>
+				<div class="form-group">
+					<label for="Organisator">Organisator:</label>
+				<div class="col-sm-8">
+					<input type="name" class="form-control" id="Organisator">
+				</div>
+				<div class="form-group">
+					<label for="MinLeeftijd">Minimum leeftijd:</label>
+				<div class="col-sm-8">
+					<input type="name" class="form-control" id="MinLeeftijd">
+				</div>
+				</div>
+				<div class="form-group">
+					<label for="MaxAanwezigen">Maximum aanwezigen:</label>
+				<div class="col-sm-8">
+					<input type="name" class="form-control" id="MaxAanwezigen">
+				</div>
+				</div>
+					<button type="submit" id="submit" class="btn btn-default">Opslaan en verzenden</button>
+			</form><br>
 		<p>Klik op "opslaan en verzenden" om uw uw nieuw evenement te starten</p>
 	</div>
 </center>
