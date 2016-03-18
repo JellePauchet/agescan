8ec6d42f6c6bbccc9d4c71291282b7b950040247
Conner Maes <Conner Maes>
2016-03-18 14:20:19 +0100

    form op niew evenement

--- views/start.php
+++ views/start.php
@@ -3,7 +3,7 @@
 ?>
 <center>
 <h1 id="AgeScan" class="text-center">AgeScan</h1>
-<form action="?pagina=new" method="POST"><button type="submit" class="btn btn-primary" aria-haspopup="true" aria-expanded="false">Nieuw Evenement</button></form>
+<form class="form" action="?pagina=new" method="POST"><button type="submit" class="btn btn-primary" aria-haspopup="true" aria-expanded="false">Nieuw Evenement</button></form>
 
-<form action="?pagina=existing" method="POST"><button type="submit" class="btn btn-danger" aria-haspopup="true" aria-expanded="false">Bestaand Evenement</button></form>
+<form class ="form" action="?pagina=existing" method="POST"><button type="submit" class="btn btn-danger" aria-haspopup="true" aria-expanded="false">Bestaand Evenement</button></form>
 </center>
